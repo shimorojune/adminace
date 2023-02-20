6b20711cb8d7b2443e25e929563fe02213c19dc3
shimorojune <rohitrajnovember@gmail.com>
2023-02-20 21:39:49 +0530

    Added SideNav options

--- src/components/Custom/SideNav/SideNav.tsx
+++ src/components/Custom/SideNav/SideNav.tsx
@@ -5,6 +5,10 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Link,
+  SxProps,
+  Theme,
+  PaperProps,
 } from "@mui/material";
 import { Icon } from "@iconify/react";
 import { I8nAndMessageProps } from "types/commonTypes";
@@ -33,47 +37,68 @@ const sideNavData: SideNavDataItem[] = [
     },
     iconKey: "mdi:pencil-ruler",
   },
+  {
+    id: "admin-panel",
+    title: {
+      i8nKey: "I18N_ADMIN_PANEL",
+    },
+    iconKey: "material-symbols:admin-panel-settings",
+  },
 ];
 
-export const SideNav = (props: SideNavProps) => {
+interface SideNavListProps {}
+
+const SideNavList = (props: SideNavListProps) => {
   // PROPS
   const {} = props;
 
   // DRAW
   return (
-    <Drawer
-      variant="permanent"
-      PaperProps={{
-        style: {
-          position: "static",
-        },
-      }}
-      sx={{
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <List>
-        {sideNavData.map((sideNavDataItem) => {
-          // DATA
-          const { id, title, iconKey } = sideNavDataItem;
+    <List>
+      {sideNavData.map((sideNavDataItem) => {
+        // DATA
+        const { id, title, iconKey } = sideNavDataItem;
 
-          // VARIABLES
-          const titleString = useTranslatedString(title);
+        // VARIABLES
+        const titleString = useTranslatedString(title);
 
-          // DRAW
-          return (
-            <ListItem key={id} disablePadding>
+        // DRAW
+        return (
+          <ListItem key={id} disablePadding>
+            <Link href="/dashboard" underline="none" width="100%" color="black">
               <ListItemButton>
                 <ListItemIcon>
                   <Icon icon={iconKey} height={30} />
                 </ListItemIcon>
                 <ListItemText primary={titleString} />
               </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
+            </Link>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+
+export const SideNav = (props: SideNavProps) => {
+  // PROPS
+  const {} = props;
+
+  // COMPONENT PROPS
+  const drawerSX: SxProps<Theme> = {
+    width: "100%",
+    height: "100%",
+  };
+  const paperProps: Partial<PaperProps<"div", {}>> = {
+    style: {
+      position: "static",
+    },
+  };
+
+  // DRAW
+  return (
+    <Drawer variant="permanent" PaperProps={paperProps} sx={drawerSX}>
+      <SideNavList />
     </Drawer>
   );
 };
