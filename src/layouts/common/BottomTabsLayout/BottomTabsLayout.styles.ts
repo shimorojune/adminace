ab2066fd42974ac56be2310830ab25fdea99ed05
shimorojune <rohitrajnovember@gmail.com>
2023-02-22 20:52:41 +0530

    Improved gap in bottom tabs

--- src/layouts/common/BottomTabsLayout/BottomTabsLayout.styles.ts
+++ src/layouts/common/BottomTabsLayout/BottomTabsLayout.styles.ts
@@ -7,7 +7,7 @@ export default {
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: 1fr 50px;
-    gap: 15px;
+    gap: 10px;
   `,
   tabsContainer: css`
     width: 100%;
